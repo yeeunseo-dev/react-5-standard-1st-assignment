3a5bc34af353dbfabf1297d8f2d9b429dd8a0cec
yeeunseo-dev <anfrhrl96@gmail.com>
2024-05-14 16:00:45 +0900

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,16 +9,33 @@ function App() {
   const [users, setUsers] = useState(initialState);
 
   // TODO: 이름과 나이를 각각 상태로 정의하세요. 초기값은 빈문자열("")입니다.
+  const [name, setName] = useState(""); // 이름 상태
+  const [age, setAge] = useState(""); // 나이 상태
 
   const addUser = (e) => {
     e.preventDefault();
     // TODO: 이름과 나이가 모두 입력되지 않았을 때는 alert 처리하고 함수를 종료하세요. 논리합연산자 (||) 를 이용하세요.
+    // 이름과 나이가 모두 입력되지 않았을 때 alert 처리하고 함수를 종료
+    if (!name || !age) {
+      alert("이름과 나이를 모두 입력해주세요.");
+      return; // 함수 종료
+    }
 
     // TODO: 사용자 리스트 상태를 업데이트 하세요. spread operator 를 사용하고, 추가되는 id는 현재 시간을 밀리초 단위로 반환하는 Date.now() 를 사용하세요.
+    // 사용자 리스트 상태를 업데이트
+    setUsers([
+      ...users, // 기존의 사용자 목록
+      { id: Date.now(), name: name, age: age }, // 새로운 사용자 객체
+    ]);
+    // 입력 필드 초기화
+    setName("");
+    setAge("");
   };
 
   const removeUser = (id) => {
     // TODO: filter 메소드를 사용해서 사용자 삭제 로직을 구현해 보세요.
+    setUsers(users.filter((user) => user.id !== id));
+    // ??????
   };
 
   return (
@@ -26,13 +43,36 @@ function App() {
       <h1>사용자 리스트</h1>
       <form onSubmit={addUser}>
         {/* TODO: input 태그에 value, onChange 속성을 추가해서 이름과 나이의 상태와 상태변경 로직을 연결하세요 */}
-        <input type="text" placeholder="이름" />
-        <input type="number" placeholder="나이" />
+        <input
+          type="text"
+          placeholder="이름"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          //?????
+        />
+        <input
+          type="number"
+          placeholder="나이"
+          value={age}
+          onChange={(e) => setAge(e.target.value)}
+        />
         <button type="submit">사용자 추가</button>
       </form>
       <ul>
         {/* TODO: map 메소드를 이용해서 user 리스트를 렌더링하세요.  */}
         {/* 이름: John, 나이: 20 [삭제] 버튼이 하나의 행에 나올 수 있도록 해보세요. (hint: flex) */}
+        {users.map((user) => (
+          <li
+            key={user.id}
+            ㅌ
+            style={{ display: "flex", justifyContent: "space-between" }}
+          >
+            <span>
+              이름: {user.name}, 나이: {user.age}
+            </span>
+            <button onClick={() => removeUser(user.id)}>삭제</button>
+          </li>
+        ))}
       </ul>
     </>
   );
